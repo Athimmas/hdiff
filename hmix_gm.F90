31fc7d6fc29083e8f6b84284e189879e8a51659f
Aketh Gowda <aketh.tm@gmail.com>
2016-04-18 16:22:36 +0530

    Correction in transition_layer code

--- hmix_gm.F90
+++ hmix_gm.F90
@@ -3876,9 +3876,11 @@
               !$OMP PARALLEL DO DEFAULT(SHARED)PRIVATE(j,i)NUM_THREADS(60)SCHEDULE(DYNAMIC,16)
               do j=1,ny_block
                    do i=1,nx_block
+
+                      WORK(i,j) = c0
+ 
                       if (kk == ktp) then
                    
-                         WORK(i,j) = c0 
                          if ( COMPUTE_TLT(i,j)  .and.  K_START(i,j) <= KMT(i,j,bid)  .and. &
                          K_START(i,j) == k ) then
                          WORK(i,j) = max(SLA_SAVE(i,j,ktp,k,bid), &
